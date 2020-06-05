87271de5a8664e515b8c4963ce2de33204c446ba
Glenn Abastillas <glennabastillas@MacBook-Pro.local>
2020-06-05 12:51:57 -0400

    Moved Syllable and Mora into syllable.py

--- sounds.py
+++ sounds.py
@@ -495,19 +495,6 @@ class Sound(object):
         self._set_feature('airway', 'egressive')
 
 
-class Mora():
-
-    def __init__(self):
-        self.weight = 1
-
-    def add(self):
-        self.weight += 1
-
-    def remove(self):
-        if self.weight > -1:
-            self.weight -= 1
-
-
 class Consonant(Sound):
 
     def __init__(self, *features, **kwargs):
@@ -616,60 +603,6 @@ class Vowel(Sound):
         self.__init__(*features, **kwargs)
 
 
-class Syllable(Sound, Mora):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__()
-        self._parse(*args, **kwargs)
-
-    def __repr__(self):
-        syllable = [__.type for __ in self.syllable]
-        return f"Syllable( {''.join(syllable)} )"
-
-    def __getitem__(self, index):
-        return self.syllable[index]
-
-    def _parse(self, *args, **kwargs):
-        ''' Parse and detect the sound types that structure this syllable '''
-
-        nucleus = 'v'
-        if args:
-            if len(args) == 1:
-                syllable = args[0].lower()
-
-            else:
-                syllable = ''.join(args).lower()
-
-            nucleus *= syllable.count(nucleus)
-
-            if not nucleus:
-                error = (
-                    'Unable to determine nucleus. No vowel nucleus detected. '
-                    'Define this syllable with a vowel nucleus `v` or using keyword arguments '
-                    'like `onset`, `nucleus`, and `coda` instead (e.g., Syllable(onset="cc", '
-                    'nucleus="c") if the nucleus is a consonant.'
-                )
-                raise ValueError(error)
-
-            onset, *_, coda = syllable.split(nucleus)
-        
-        elif kwargs:
-            onset = kwargs.get('onset', '').lower()
-            nucleus = kwargs.get('nucleus', 'v').lower()
-            coda = kwargs.get('coda', '').lower()
-    
-        self.onset = [Consonant(__) if __ == 'c' else Vowel(__) for __ in onset]
-        self.nucleus = [Consonant(__) if __ == 'c' else Vowel(__) for __ in nucleus]
-        self.coda = [Consonant(__) if __ == 'c' else Vowel(__) for __ in coda]
-
-        self.body = self.onset + self.nucleus
-        self.rhyme = self.nucleus + self.coda
-        self.syllable = self.body + self.coda
-
-    def randomize(self):
-        ''' IN DEVELOPMENT USE WITH SOUNDS.YAML '''
-        if self.syllable:
-            pass
 
 if __name__ == '__main__':
     c = Sound('sh')
--- syllable.py
+++ syllable.py
@@ -1,3 +1,79 @@
-from configuration import Sounds
-import random
+from resource import SoundsResource
+from sounds import Sound, Consonant, Vowel
 
+SNDS = SoundsResource()
+
+
+class Mora():
+
+    def __init__(self):
+        self.weight = 1
+
+    def add(self):
+        self.weight += 1
+
+    def remove(self):
+        if self.weight > -1:
+            self.weight -= 1
+
+
+class Syllable(Sound, Mora):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self._parse(*args, **kwargs)
+
+    def __repr__(self):
+        syllable = [__.type for __ in self.syllable]
+        return f"Syllable( {''.join(syllable)} )"
+
+    def __getitem__(self, index):
+        return self.syllable[index]
+
+    def _parse(self, *args, **kwargs):
+        ''' Parse and detect the sound types that structure this syllable '''
+
+        nucleus = 'v'
+        if args:
+            if len(args) == 1:
+                syllable = args[0].lower()
+
+            else:
+                syllable = ''.join(args).lower()
+
+            nucleus *= syllable.count(nucleus)
+
+            if not nucleus:
+                error = (
+                    'Unable to determine nucleus. No vowel nucleus detected. '
+                    'Define this syllable with a vowel nucleus `v` or using keyword arguments '
+                    'like `onset`, `nucleus`, and `coda` instead (e.g., Syllable(onset="cc", '
+                    'nucleus="c") if the nucleus is a consonant.'
+                )
+                raise ValueError(error)
+
+            onset, *_, coda = syllable.split(nucleus)
+        
+        elif kwargs:
+            onset = kwargs.get('onset', '').lower()
+            nucleus = kwargs.get('nucleus', 'v').lower()
+            coda = kwargs.get('coda', '').lower()
+
+        classify = lambda letters : [Consonant(__) if __.startswith('c') else Vowel(__) for __ in letters]
+    
+        self.onset = classify(onset)
+        self.nucleus = classify(nucleus)
+        self.coda = classify(coda)
+
+        self.body = self.onset + self.nucleus
+        self.rhyme = self.nucleus + self.coda
+        self.syllable = self.body + self.coda
+
+    def randomize(self):
+        ''' IN DEVELOPMENT USE WITH SOUNDS.YAML '''
+        if self.syllable:
+            pass
+
+if __name__ == "__main__":
+    syl = Syllable('cvc')
+    print(syl)
