e767b70f78a979d0d7bdccc0ec81643c598cabdf
Glenn Abastillas <glennabastillas@MacBook-Pro.local>
2020-06-12 23:55:53 -0400

    Added IPA data to resources/lang folders

--- resource.py
+++ resource.py
@@ -11,6 +11,19 @@ from collections import namedtuple, defaultdict
 import yaml
 
 
+def process_ipa(text, filename):
+    import re
+    dots = re.compile(r'(.)([:ː])', re.I)
+    ipa = re.compile(r'/(.+?)/', re.I)
+
+    text = dots.sub(r'\1\1', text)
+    text = ipa.findall(text)
+
+    with open(filename, 'w') as fout:
+        text = '\n'.join(text)
+        fout.write(text)
+
+
 class Resource():
 
     def __init__(self, resource='phonology'):
